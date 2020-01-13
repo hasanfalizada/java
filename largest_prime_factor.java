3140b2804167a5d4b1338f9175f19247d3882ab5
Hasan Alizada <ha@hasanalizada.net>
2020-01-13 23:14:46 +0400

    !

--- largest_prime_factor.java
+++ largest_prime_factor.java
@@ -1,16 +1,16 @@
 class T {
-    static boolean isPrime(int factor) {
-        int counter = 0;
-        for (int i = 1; i < factor; i++) {
-            if (factor % i == 0) {
-                counter++;
-            }
-            if (counter >= 2) {
-                return false;
-            }
-        }
-        return true;
-    }
+	static boolean isPrime(int factor) {
+		int counter = 0;
+		for (int i = 1; i <= Math.sqrt(factor); i++) {
+			if (factor % i == 0) {
+				counter++;
+			}
+			if (counter >= 2) {
+				return false;
+			}
+		}
+		return true;
+	}
 
     public static void main(String[] args) {
         double val = 600851475143.0;
