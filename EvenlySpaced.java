25e3344a68b570fba53580f177317f7d0e3b389a
Hasan Alizada <ha@hasanalizada.net>
2020-06-23 22:52:15 +0400

    !

---
+++ EvenlySpaced.java
@@ -0,0 +1,26 @@
+package devjava;
+
+public class EvenlySpaced {
+	public boolean evenlySpaced(int a, int b, int c) {
+		int temp;
+		if (a > b) {
+			temp = a;
+			a = b;
+			b = temp;
+		}
+
+		if (b > c) {
+			temp = b;
+			b = c;
+			c = temp;
+		}
+
+		if (a > b) {
+			temp = a;
+			a = b;
+			b = temp;
+		}
+
+		return b - a == c - b;
+	}
+}
